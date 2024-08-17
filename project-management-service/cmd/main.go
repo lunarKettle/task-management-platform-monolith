2b2fa3f49a04229fec8d64b4bedbf746896d1948
lunarKettle <1evgeny.nesterenko1@gmail.com>
2024-08-17 18:29:06 +0300

    add GRPCServer type in project management microservice

--- project-management-service/cmd/main.go
+++ project-management-service/cmd/main.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"project-management-service/internal/grpc_handler"
+)
+
+func main() {
+	grpc_handler.NewGRPCServer()
+}
---
+++ project-management-service/internal/grpc_handler/grpc_server.go
@@ -0,0 +1,39 @@
+package grpc_handler
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "project-management-service/proto"
+
+	"google.golang.org/grpc"
+)
+
+type GRPCServer struct {
+	pb.ProjectManagementServiceServer
+}
+
+func NewGRPCServer() *GRPCServer {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	server := &GRPCServer{}
+	pb.RegisterProjectManagementServiceServer(s, server)
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+	return server
+}
+
+func (s *GRPCServer) GetProject(ctx context.Context, request *pb.ProjectRequest) (*pb.ProjectResponse, error) {
+	return &pb.ProjectResponse{
+		ProjectName:        "test name",
+		ProjectDescription: "test descrition",
+	}, nil
+}
