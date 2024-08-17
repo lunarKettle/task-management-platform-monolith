06e9107a18d076c6a16186154c6de244d15c4679
lunarKettle <1evgeny.nesterenko1@gmail.com>
2024-08-17 18:27:57 +0300

    add GRPCClient type in api-gateway microservice

--- api-gateway/cmd/main.go
+++ api-gateway/cmd/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"api-gateway/internal/handler"
-	"fmt"
+	"api-gateway/internal/grpc_client"
+	"log"
+	"time"
 )
 
 func main() {
-	server := handler.NewServer(":8080")
-	fmt.Printf("Starting server at %s\n", server.Address)
-	if err := server.Start(); err == nil {
-		fmt.Println("Ошибка")
-	}
+	// server := handler.NewServer(":8080")
+	// fmt.Printf("Starting server at %s\n", server.Address)
+	// if err := server.Start(); err == nil {
+	// 	fmt.Println("Ошибка")
+	// }
+
+	client := grpc_client.NewGRPCClient()
+	r, _ := client.GetProject()
+	log.Printf("Projects: %s", r.GetProjectDescription())
+	time.Sleep(10 * time.Second)
 }
---
+++ api-gateway/internal/grpc_client/grpc_client.go
@@ -0,0 +1,44 @@
+package grpc_client
+
+import (
+	pb "api-gateway/proto"
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type GRPCClient struct {
+	client pb.ProjectManagementServiceClient
+	conn   *grpc.ClientConn
+}
+
+func NewGRPCClient() *GRPCClient {
+	const (
+		address = "localhost:50051"
+	)
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	c := pb.NewProjectManagementServiceClient(conn)
+	return &GRPCClient{c, conn}
+}
+
+func (g *GRPCClient) Close() {
+	if err := g.conn.Close(); err != nil {
+		log.Fatalf("failed to close connection: %v", err)
+	}
+}
+
+func (g *GRPCClient) GetProject() (*pb.ProjectResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := g.client.GetProject(ctx, &pb.ProjectRequest{})
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	return r, err
+}
