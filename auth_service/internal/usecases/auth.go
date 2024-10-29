beeff8945bbe2ed7a60aeda9cbdafa84ac9a1ba5
lunarKettle <1evgeny.nesterenko1@gmail.com>
2024-10-30 00:10:37 +0300

    add basic usecases

---
+++ auth_service/internal/usecases/auth.go
@@ -0,0 +1,80 @@
+package usecases
+
+import (
+	"auth_service/internal/common"
+	"auth_service/internal/models"
+	"errors"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthUseCases struct {
+	repo           UserRepository
+	tokenGenerator TokenGenerator
+}
+
+func NewAuthUseCases(repo UserRepository, tokenGenerator TokenGenerator) *AuthUseCases {
+	return &AuthUseCases{
+		repo:           repo,
+		tokenGenerator: tokenGenerator,
+	}
+}
+
+type CreateUserCommand struct {
+	username string
+	email    string
+	password string
+	role     string
+}
+
+func NewCreateUserCommand(
+	username string,
+	email string,
+	password string,
+	role string,
+) *CreateUserCommand {
+	return &CreateUserCommand{
+		username: username,
+		email:    email,
+		password: password,
+		role:     role,
+	}
+}
+
+func (a *AuthUseCases) CreateUser(cmd *CreateUserCommand) (string, error) {
+	_, err := a.repo.GetByUsername(cmd.username)
+
+	switch {
+	case errors.Is(err, common.ErrNotFound):
+	case err == nil:
+		return "", fmt.Errorf("%w: user with username %q already exists", common.ErrAlreadyExists, cmd.username)
+	default:
+		return "", fmt.Errorf("failed to get user by username %q: %w", cmd.username, err)
+	}
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(cmd.password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %w", err)
+	}
+
+	user := models.User{
+		Username:     cmd.username,
+		Email:        cmd.email,
+		PasswordHash: passwordHash,
+		Role:         cmd.role,
+	}
+
+	userId, err := a.repo.Create(user)
+	if err != nil {
+		return "", fmt.Errorf("failed to create user: %w", err)
+	}
+
+	token, err := a.tokenGenerator.GenerateToken(userId, cmd.role)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate token: %w", err)
+	}
+
+	return token, nil
+}
